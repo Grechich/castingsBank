6850856f008a103eadbd4d40bf25820b4385e171
Irina Smirnova <ira343511@gmail.com>
2022-01-17 13:49:25 +0200

    deploy on Heroku

--- src/main/java/com/gmail/grechich/security/MyController.java
+++ src/main/java/com/gmail/grechich/security/MyController.java
@@ -9,12 +9,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.List;
 
-@RestController
+@Controller
 public class MyController {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
@@ -57,7 +56,7 @@ public class MyController {
 
     @GetMapping("/login")
     public String loginPage() {
-        return "login";
+        return "login1";
     }
 
     @GetMapping("/register")
