ef2251f8958a3b79fbda35b2e28c37988dbd3789
Irina Smirnova <ira343511@gmail.com>
2022-01-18 10:32:30 +0200

    deploy on Heroku

--- src/main/java/com/gmail/grechich/security/MyController.java
+++ src/main/java/com/gmail/grechich/security/MyController.java
@@ -25,11 +25,11 @@ public class MyController {
 
     @GetMapping("/")
     public String index(Model model){
-        User user = getCurrentUser();
-        String login = user.getUsername();
-        model.addAttribute("login", login);
-        model.addAttribute("roles", user.getAuthorities());
-        model.addAttribute("admin", isAdmin(user));
+//        User user = getCurrentUser();
+//        String login = user.getUsername();
+//        model.addAttribute("login", login);
+//        model.addAttribute("roles", user.getAuthorities());
+//        model.addAttribute("admin", isAdmin(user));
         return "index";
     }
 
