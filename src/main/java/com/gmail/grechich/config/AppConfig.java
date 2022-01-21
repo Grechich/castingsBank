09cd8f5d2ae1c60b3caffbf79cf69fa70efa2e96
Irina Smirnova <ira343511@gmail.com>
2022-01-21 10:38:26 +0200

    Heroku with Postgres

--- src/main/java/com/gmail/grechich/config/AppConfig.java
+++ src/main/java/com/gmail/grechich/config/AppConfig.java
@@ -1,5 +1,8 @@
 package com.gmail.grechich.config;
 
+import com.gmail.grechich.security.UserRole;
+import com.gmail.grechich.security.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -11,8 +14,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AppConfig extends GlobalMethodSecurityConfiguration {
 
+    public static final String ADMIN = "admin";
+    public static final String USER = "user";
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public CommandLineRunner demo(final UserService userService,
+                                  final PasswordEncoder encoder) {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... strings) throws Exception {
+                userService.addUser(ADMIN,
+                        encoder.encode("111"),
+                        UserRole.ADMIN);
+
+                userService.addUser(USER,
+                        encoder.encode("111"),
+                        UserRole.USER);
+            }
+        };
+    }
+
 }
